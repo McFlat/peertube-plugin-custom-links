1d0e77d27db5f559beb8dd771a80c2e830f6add3
McFlat <nopsidy@gmail.com>
2020-05-31 19:20:39 -0500

    update

--- client/common-client-plugin.js
+++ client/common-client-plugin.js
@@ -8,15 +8,15 @@ function register ({ registerHook, peertubeHelpers }) {
 
       const panel = document.createElement('div');
       panel.setAttribute('class', 'panel-block');
-      const title = document.createElement('div');
-      title.setAttribute('class', 'block-title');
+      //const title = document.createElement('div');
+      //title.setAttribute('class', 'block-title');
 
       let newLink, newText;
-      newText = document.createTextNode(c.title || 'Links');
-      title.appendChild(newText);
-      panel.appendChild(title);
+      //newText = document.createTextNode(c.title || 'Links');
+      //title.appendChild(newText);
+      //panel.appendChild(title);
 
-      if (c.link_1_text && c.link_1_url) {
+      if (document.querySelector('.custom-link-1') === null && c.link_1_text && c.link_1_url) {
         newLink = document.createElement('a');
         newLink.setAttribute('class', 'custom-link custom-link-1');
         newLink.setAttribute('href', c.link_1_url);
@@ -24,7 +24,7 @@ function register ({ registerHook, peertubeHelpers }) {
         newLink.appendChild(newText);
         panel.appendChild(newLink);
       }
-      if (c.link_2_text && c.link_2_url) {
+      if (document.querySelector('.custom-link-2') === null && c.link_2_text && c.link_2_url) {
         newLink = document.createElement('a');
         newLink.setAttribute('class', 'custom-link custom-link-2');
         newLink.setAttribute('href', c.link_2_url);
@@ -32,7 +32,7 @@ function register ({ registerHook, peertubeHelpers }) {
         newLink.appendChild(newText);
         panel.appendChild(newLink);
       }
-      if (c.link_3_text && c.link_3_url) {
+      if (document.querySelector('.custom-link-3') === null && c.link_3_text && c.link_3_url) {
         newLink = document.createElement('a');
         newLink.setAttribute('class', 'custom-link custom-link-3');
         newLink.setAttribute('href', c.link_3_url);
@@ -40,7 +40,7 @@ function register ({ registerHook, peertubeHelpers }) {
         newLink.appendChild(newText);
         panel.appendChild(newLink);
       }
-      if (c.link_4_text && c.link_4_url) {
+      if (document.querySelector('.custom-link-4') === null && c.link_4_text && c.link_4_url) {
         newLink = document.createElement('a');
         newLink.setAttribute('class', 'custom-link custom-link-4');
         newLink.setAttribute('href', c.link_4_url);
@@ -48,7 +48,7 @@ function register ({ registerHook, peertubeHelpers }) {
         newLink.appendChild(newText);
         panel.appendChild(newLink);
       }
-      if (c.link_5_text && c.link_5_url) {
+      if (document.querySelector('.custom-link-5') === null && c.link_5_text && c.link_5_url) {
         newLink = document.createElement('a');
         newLink.setAttribute('class', 'custom-link custom-link-5');
         newLink.setAttribute('href', c.link_5_url);
