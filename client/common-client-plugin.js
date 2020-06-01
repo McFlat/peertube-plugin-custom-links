6e01b60afc84b7389d0db2783142773179813807
McFlat <nopsidy@gmail.com>
2020-05-31 20:33:55 -0500

    update

--- client/common-client-plugin.js
+++ client/common-client-plugin.js
@@ -4,58 +4,14 @@ function register ({ registerHook, peertubeHelpers }) {
     handler: async ({ path }) => {
       const component = document.querySelector('.top-menu');
 
-      const c = await peertubeHelpers.getSettings()
-      const panel = document.createElement('div');
-      panel.setAttribute('class', 'panel-block');
-      //const title = document.createElement('div');
-      //title.setAttribute('class', 'block-title');
-
-      let newLink, newText;
-      //newText = document.createTextNode(c.title || 'Links');
-      //title.appendChild(newText);
-      //panel.appendChild(title);
-
-      if (document.querySelector('.custom-link-1') === null && c.link_1_text && c.link_1_url) {
-        newLink = document.createElement('a');
-        newLink.setAttribute('class', 'custom-link custom-link-1');
-        newLink.setAttribute('href', c.link_1_url);
-        newText = document.createTextNode(c.link_1_text);
-        newLink.appendChild(newText);
-        panel.appendChild(newLink);
+      const c = await peertubeHelpers.getSettings('custom_links_markdown')
+      if (document.querySelector('.custom-links') === null && c) {
+        const panel = document.createElement('div');
+        panel.setAttribute('class', 'panel-block custom-links');
+        const html = await peertubeHelpers.markdownRenderer.enhancedMarkdownToHTML(c)
+        panel.appendChild(html);
+        component.appendChild(panel);
       }
-      if (document.querySelector('.custom-link-2') === null && c.link_2_text && c.link_2_url) {
-        newLink = document.createElement('a');
-        newLink.setAttribute('class', 'custom-link custom-link-2');
-        newLink.setAttribute('href', c.link_2_url);
-        newText = document.createTextNode(c.link_2_text);
-        newLink.appendChild(newText);
-        panel.appendChild(newLink);
-      }
-      if (document.querySelector('.custom-link-3') === null && c.link_3_text && c.link_3_url) {
-        newLink = document.createElement('a');
-        newLink.setAttribute('class', 'custom-link custom-link-3');
-        newLink.setAttribute('href', c.link_3_url);
-        newText = document.createTextNode(c.link_3_text);
-        newLink.appendChild(newText);
-        panel.appendChild(newLink);
-      }
-      if (document.querySelector('.custom-link-4') === null && c.link_4_text && c.link_4_url) {
-        newLink = document.createElement('a');
-        newLink.setAttribute('class', 'custom-link custom-link-4');
-        newLink.setAttribute('href', c.link_4_url);
-        newText = document.createTextNode(c.link_4_text);
-        newLink.appendChild(newText);
-        panel.appendChild(newLink);
-      }
-      if (document.querySelector('.custom-link-5') === null && c.link_5_text && c.link_5_url) {
-        newLink = document.createElement('a');
-        newLink.setAttribute('class', 'custom-link custom-link-5');
-        newLink.setAttribute('href', c.link_5_url);
-        newText = document.createTextNode(c.link_5_text);
-        newLink.appendChild(newText);
-        panel.appendChild(newLink);
-      }
-      component.appendChild(panel);
     }
   })
 }
--- main.js
+++ main.js
@@ -8,69 +8,9 @@ async function register ({
   videoLanguageManager
 }) {
   registerSetting({
-    name: 'title',
-    label: 'Links Block Title',
-    type: 'input',
-    private: false,
-    default: 'Links'
-  }, {
-    name: 'link_1_text',
-    label: 'Link 1 Text',
-    type: 'input',
-    private: false,
-    default: ''
-  }, {
-    name: 'link_1_url',
-    label: 'Link 1 URL',
-    type: 'input',
-    private: false,
-    default: ''
-  }, {
-    name: 'link_2_text',
-    label: 'Link 2 Text',
-    type: 'input',
-    private: false,
-    default: ''
-  }, {
-    name: 'link_2_url',
-    label: 'Link 2 URL',
-    type: 'input',
-    private: false,
-    default: ''
-  }, {
-    name: 'link_3_text',
-    label: 'Link 3 Text',
-    type: 'input',
-    private: false,
-    default: ''
-  }, {
-    name: 'link_3_url',
-    label: 'Link 3 URL',
-    type: 'input',
-    private: false,
-    default: ''
-  }, {
-    name: 'link_4_text',
-    label: 'Link 4 Text',
-    type: 'input',
-    private: false,
-    default: ''
-  }, {
-    name: 'link_4_url',
-    label: 'Link 4 URL',
-    type: 'input',
-    private: false,
-    default: ''
-  }, {
-    name: 'link_5_text',
-    label: 'Link 5 Text',
-    type: 'input',
-    private: false,
-    default: ''
-  }, {
-    name: 'link_5_url',
-    label: 'Link 5 URL',
-    type: 'input',
+    name: 'custom_links_markdown',
+    label: 'Custom Links Markdown',
+    type: 'markdown-enhanced',
     private: false,
     default: ''
   })
