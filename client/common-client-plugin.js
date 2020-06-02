8583defb1b1953a96ef38b630a0033ba67c8be9a
McFlat <nopsidy@gmail.com>
2020-06-01 22:49:38 -0500

    0.0.10

--- client/common-client-plugin.js
+++ client/common-client-plugin.js
@@ -2,17 +2,28 @@ function register ({ registerHook, peertubeHelpers }) {
   registerHook({
     target: 'action:router.navigation-end',
     handler: async ({ path }) => {
-      const component = document.querySelector('.top-menu');
-
       const c = await peertubeHelpers.getSettings('custom_links_markdown')
-      
-      if (document.querySelector('.custom-links') === null && c['custom_links_markdown']) {
-        const panel = document.createElement('div');
-        panel.setAttribute('class', 'panel-block custom-links');
+      const panel = document.createElement('div');
+      panel.setAttribute('class', 'custom-links');
+      if (c['custom_links_markdown']) {
         const html = await peertubeHelpers.markdownRenderer.enhancedMarkdownToHTML(c['custom_links_markdown'])
         panel.innerHTML = html;
-        component.appendChild(panel);
       }
+      setInterval(async function(){
+        if (document.querySelector('.top-menu .custom-links') === null && c['custom_links_markdown']) {
+          const topMenu = document.querySelector('.top-menu');
+          if (topMenu) {
+            topMenu.appendChild(panel);
+          }
+        }
+        if (document.querySelector('.menu-wrapper .custom-links') === null && c['custom_links_markdown']) {
+          const mainContent = document.querySelector('.menu-wrapper');
+          if (mainContent) {
+            panel.classList.add('section')
+            mainContent.appendChild(panel)
+          }
+        }
+      }, 1)
     }
   })
 }
