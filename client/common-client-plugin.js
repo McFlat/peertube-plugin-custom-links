442e279aba30d83a49eeed0934c02d868af5e283
Raphael Dannecker <dannecker@schule-bw.de>
2021-03-23 21:50:05 +0000

    link target _self

--- client/common-client-plugin.js
+++ client/common-client-plugin.js
@@ -7,7 +7,7 @@ function register ({ registerHook, peertubeHelpers }) {
       panel.setAttribute('class', 'custom-links');
       if (c['custom_links_markdown']) {
         const html = await peertubeHelpers.markdownRenderer.enhancedMarkdownToHTML(c['custom_links_markdown'])
-        panel.innerHTML = html;
+        panel.innerHTML = html.replaceAll("_blank","_self");
       }
       setInterval(async function(){
         if (document.querySelector('.top-menu .custom-links') === null && c['custom_links_markdown']) {
